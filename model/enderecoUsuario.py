4d7ed54b3dcabcdc03333f070a1d3b8e19198a26
marcellacg <marcelladacosta@gmail.com>
2023-04-16 18:23:13 -0300

    alteracoes em model enderecoUsuario login e usuario

---
+++ model/enderecoUsuario.py
@@ -0,0 +1,33 @@
+from helpers.database import db
+from flask_restful import fields
+
+
+endereco_fields = {
+    'id': fields.Integer(attribute='id'),
+    'logradouro': fields.String(attribute='logradouro'),
+    'numero': fields.String(attribute='numero'),
+    'cidade': fields.String(attribute='cidade'),
+    'estado': fields.String(attribute='estado')
+}
+
+
+class EnderecoUsuario(db.Model):
+
+    __tablename__ = "tb_enderecoUsuario"
+
+    id = db.Column(db.Integer, primary_key=True)
+    logradouro = db.Column(db.String, nullable=False)
+    numero = db.Column(db.String(9), nullable=False)
+    cidade = db.Column(db.String(), nullable=False)
+    estado = db.Column(db.String(), nullable=False)
+
+    usuario_id = db.Column(db.Integer, db.ForeignKey("tb_usuario.id"))
+
+    def __init__(self, logradouro, numero, cidade, estado):
+        self.logradouro = logradouro
+        self.numero = numero
+        self.cidade = cidade
+        self.estado = estado
+
+    def __repr__(self):
+        return '<Logradouro: {}\n Numero: {}\n Cidade: {}\n Estado: {}>'.format(self.logradouro, self.numero, self.cidade, self.estado)
---
+++ model/usuario.py
@@ -0,0 +1,42 @@
+from sqlalchemy import ForeignKey
+from helpers.database import db
+from sqlalchemy.types import String
+from flask_restful import fields
+from model.enderecoUsuario import endereco_fields
+
+usuario_fields = {
+    'id': fields.Integer(attribute='id'),
+    'email': fields.String(attribute='email'),
+    'senha': fields.String(attribute='senha'),
+    'endereco': fields.Nested(endereco_fields),
+    'telefone': fields.String(attribute='telefone'),
+    'observacoes': fields.String(attribute='observacoes')
+}
+
+class Usuario(db.Model):
+
+    __tablename__ = "tb_usuario"
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    nome = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True)
+    senha = db.Column(db.String(30), unique=True, nullable=False)
+    telefone = db.Column(db.String(11))
+    observacoes = db.Column(db.String(700))
+
+    EnderecoUsuario = db.relationship("EnderecoUsuario", uselist=False)
+
+    # Herança: Superclasse
+    tipo_usuario = db.Column('tipo_usuario', String(50))
+    __mapper_args__ = {'polymorphic_on': tipo_usuario}
+
+    def __init__(self, nome, email, senha, telefone, endereco: EnderecoUsuario, observacoes):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.telefone = telefone
+        self.endereco = endereco
+        self.observacoes = observacoes
+
+    def __repr__(self):
+        return '<Nome: {}\n Email: {}\n Telefone: {}\n Observacoes: {}>'.format(self.nome, self.email, self.telefone, self.observacoes)
